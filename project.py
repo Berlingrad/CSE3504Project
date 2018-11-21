34d373d588c2d793fd806955beb77ff981405e56
berlingrad <stormrisexu@hotmail.com>
2018-11-21 17:15:22 -0500

    0.2

--- project.py
+++ project.py
@@ -1,17 +1,19 @@
-import time
+import time, copy
 
 testcase1 = [[12, 55, 67], [4, 71, 59], [20, 67, 70], [92, 68, 90], [83, 54, 13]]
 
 def LRJF(works):
     start_time = time.time()
-    quickSort(works, 0, len(works)-1, 2)
+    output = copy.deepcopy(works)
+    quickSort(output, 0, len(works)-1, 2)
     print("LRJF runtime = %s seconds" % (time.time() - start_time))
-    return works
+    return output
 def FCFS(works):
     start_time = time.time()
+    output = copy.deepcopy(works)
     quickSort(works, 0, len(works)-1, 0)
     print("FCFS runtime = %s seconds" % (time.time() - start_time))
-    return works
+    return output
 
 def quickSort(m, first, last, vtc):
     if first < last:
@@ -43,12 +45,24 @@ def partition(m, first, last, vtc):
 
 def totalTime(m):
     output = []
+    for i, col in enumerate(m):
+        sum = col[0] + col[2]
+        for j in range(i+1):
+            sum += m[j][1]
+        output.append(sum)
+    return(output)
 
 
-LRJF(testcase1)
+
+out = LRJF(testcase1)
 
 for i in range(3):
-    for col in testcase1:
+    for col in out:
         print(col[i], end="\t")
     print("\n")
+
+print("Total time cost of each branch: ")
+for n in totalTime(out):
+    print(n, end = "\t")
+print("\n")
 
